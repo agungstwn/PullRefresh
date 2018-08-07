939797e38c168bc5503729332d506c4c0965039c
Agung <agungstwn0@gmail.com>
2018-08-07 10:32:49 +0700

    first commit

---
+++ app/src/main/java/id/agung/android/refreshlayout/App/MainActivity.java
@@ -0,0 +1,15 @@
+package id.agung.android.refreshlayout.App;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.agung.android.refreshlayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
