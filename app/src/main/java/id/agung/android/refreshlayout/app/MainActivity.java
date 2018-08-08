5ef9bf9d8f9085ccb7b832b0c3eb5deb9781af8a
Agung <agungstwn0@gmail.com>
2018-08-08 17:51:56 +0700

    add progress bar horizontal

--- app/src/main/java/id/agung/android/refreshlayout/app/MainActivity.java
+++ app/src/main/java/id/agung/android/refreshlayout/app/MainActivity.java
@@ -2,14 +2,28 @@ package id.agung.android.refreshlayout.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import id.agung.android.refreshlayout.R;
 import id.agung.android.refreshlayout.app.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
+    @BindView(R.id.progressbar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.textview)
+    TextView textView;
+    @BindView(R.id.m_btn_progressbar)
+    Button mBtnProgressbar;
+
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,34 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         homeToolbar("Home");
+
+        mBtnProgressbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressStatus = 0;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (progressStatus < 100){
+                            progressStatus += 1;
+                            try {
+                                Thread.sleep(20);
+                            }catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(progressStatus);
+                                    textView.setText(progressStatus + "");
+                                }
+                            });
+                        }
+                    }
+                }).start();
+            }
+        });
     }
 
     @OnClick({R.id.m_btn_listview, R.id.m_btn_recyclerview, R.id.m_btn_scrollview})
--- app/src/main/java/id/agung/android/refreshlayout/app/ScrollViewActivity.java
+++ app/src/main/java/id/agung/android/refreshlayout/app/ScrollViewActivity.java
@@ -1,17 +1,41 @@
 package id.agung.android.refreshlayout.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.baoyz.widget.PullRefreshLayout;
+import com.baoyz.widget.SmartisanDrawable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import id.agung.android.refreshlayout.R;
 import id.agung.android.refreshlayout.app.base.BaseActivity;
 
 public class ScrollViewActivity extends BaseActivity {
+
+    @BindView(R.id.swipe_refresh_layout)
+    PullRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_view);
         initToolbar("Scroll View");
+        ButterKnife.bind(this);
+
+        mSwipeRefreshLayout.setOnRefreshListener(new PullRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSwipeRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 3000);
+            }
+        });
+
+        mSwipeRefreshLayout.setColorSchemeColors(Color.BLUE);
+        mSwipeRefreshLayout.setRefreshDrawable(new SmartisanDrawable(this, mSwipeRefreshLayout));
     }
 }
